73876a307a89c7693f6f61575633dfcb8ef8b715
Lingyao <125314658+Lingyao1219@users.noreply.github.com>
2024-05-26 20:07:12 -0400

    Add files via upload
    Add word_list

---
+++ word_list.py
@@ -0,0 +1,158 @@
+damage_words_english = [
+    "blackout",
+    "broke",
+    "broken",
+    "burn",
+    "burned",
+    "burning",
+    "burns",
+    "catastrophe",
+    "catastrophes",
+    "catastrophic",
+    "chaos",
+    "collapse",
+    "collapsed",
+    "collapses",
+    "crack",
+    "cracked",
+    "cracking",
+    "cracks",
+    "crash",
+    "crashed",
+    "crashes",
+    "cripple",
+    "cripples",
+    "crumble",
+    "crush",
+    "crushed",
+    "crushes",
+    "damage",
+    "damaged",
+    "damaging",
+    "dead",
+    "death",
+    "deaths",
+    "deform",
+    "deformed",
+    "deforms",
+    "demonish",
+    "destruct",
+    "destructed",
+    "destructing",
+    "destructs",
+    "destroy",
+    "destroyed",
+    "destroying",
+    "destroys",
+    "devastate",
+    "devastated",
+    "devastates",
+    "devastating",
+    "die",
+    "died",
+    "dies",
+    "displace",
+    "displaced",
+    "disrupt",
+    "disrupted",
+    "disrupting",
+    "disrupts",
+    "fatalities",
+    "fatality",
+    "fissure",
+    "fissures",
+    "fire",
+    "flood",
+    "flooded",
+    "flooding",
+    "hurt",
+    "hurting",
+    "hurts",
+    "injuries",
+    "injured",
+    "injury",
+    "kill",
+    "killed",
+    "killing",
+    "leak",
+    "leaked",
+    "leaking",
+    "leaks",
+    "massive",
+    "outage",
+    "rockslide",
+    "rubble",
+    "rupture",
+    "ruptures",
+    "safe",
+    "safety",
+    "scatter",
+    "scattered",
+    "scatters",
+    "severe",
+    "shatter",
+    "shattered",
+    "shatters",
+    "smash",
+    "smashed",
+    "smashes",
+    "smashing",
+    "suffer",
+    "suffered",
+    "suffering",
+    "suffers",
+    "trauma",
+    "warp",
+    "warps",
+    "wreck",
+    "wrecked",
+    "wrecks"
+]
+
+damage_words_japanese = [
+    "停電",       # blackout
+    "壊れた",     # broke, broken
+    "燃える",     # burn
+    "燃えた",     # burned
+    "燃えている",  # burning
+    "大災害",     # catastrophe, catastrophes
+    "壊滅的",     # catastrophic
+    "混乱",       # chaos
+    "崩壊",       # collapse, collapsed, collapses
+    "ひび",       # crack, cracked, cracking, cracks
+    "墜落",       # crash, crashed, crashes
+    "無力",       # cripple, cripples
+    "崩れる",     # crumble
+    "押しつぶす",  # crush, crushed, crushes
+    "損傷",       # damage, damaged, damaging
+    "死んだ",     # dead, died, die, dies
+    "死亡",       # death, deaths
+    "変形する",    # deform, deformed, deforms
+    "破壊",       # destruct, destructed, destructing, destructs
+    "破壊する",    # destroy, destroyed, destroying, destroys
+    "壊滅させる",  # devastate, devastated, devastates, devastating
+    "死ぬ",       # die, died, dies
+    "避難する",    # displace, displaced
+    "混乱する",    # disrupt, disrupted, disrupting, disrupts
+    "死者",       # fatalities, fatality
+    "裂け目",     # fissure, fissures
+    "火事",       # fire
+    "洪水",       # flood, flooded, flooding
+    "傷つく",     # hurt, hurting, hurts
+    "けが",       # injuries, injury
+    "負傷した",    # injured
+    "殺す",       # kill, killed, killing
+    "漏れ",       # leak, leaked, leaking, leaks
+    "巨大な",     # massive
+    "がけ崩れ",   # rockslide
+    "瓦礫",       # rubble
+    "破裂",       # rupture, ruptures
+    "安全",       # safe
+    "散らす",     # scatter, scattered, scatters
+    "厳しい",     # severe
+    "粉々にする",  # shatter, shattered, shatters
+    "打ち砕く",   # smash, smashed, smashes, smashing
+    "苦しむ",     # suffer, suffered, suffering, suffers
+    "外傷",       # trauma
+    "ゆがむ",     # warp, warps
+]
